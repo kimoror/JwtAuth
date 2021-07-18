69bd8a15a850df4925a0cc9c8087e759b983b143
kimoror <kimoror@mail.ru>
2021-07-19 00:19:22 +0300

    fix auth.module.js

--- Frontend/vue/src/store/auth.module.js
+++ Frontend/vue/src/store/auth.module.js
@@ -6,13 +6,14 @@ const initialState = user
 : { status: { loggedIn: false}, user: null};
 
 export const auth = {
+    namespaced: true,
     state: initialState,
     mutations: {
         loginSuccess(state, user) {
             state.status.loggedIn = true
             state.user = user
         },
-        loginFailure(state){
+        loginFailure(state) {
             state.status.loggedIn = false;
             state.user = null;
         },
@@ -26,35 +27,35 @@ export const auth = {
         registerFailure(state) {
             state.status.loggedIn = false;
         },
-        actions: {
-            login({ commit }, user) {
-                return AuthService.login(user).then(
-                    user => {
-                        commit('loginSuccess', user);
-                        return Promise.resolve(user);
-                    },
-                    error => {
-                        commit('loginFailure')
-                        return Promise.reject(error);
-                    }
-                );
-            },
-            logout({ commit }){
-                AuthService.logout();
-                commit('logout');
-            },
-            register({ commit }, user) {
-                return AuthService.register(user).then(
-                    response => {
-                        commit('registerSuccess');
-                        return Promise.resolve(response.data);
-                    },
-                    error => {
-                        commit('registerFailure');
-                        return Promise.reject(error)
-                    }
-                );
-            }
+    },
+    actions: {
+        login({ commit }, user) {
+            return AuthService.login(user).then(
+                user => {
+                    commit('loginSuccess', user);
+                    return Promise.resolve(user);
+                },
+                error => {
+                    commit('loginFailure')
+                    return Promise.reject(error);
+                }
+            );
+        },
+        logout({ commit }){
+            AuthService.logout();
+            commit('logout');
+        },
+        register({ commit }, user) {
+            return AuthService.register(user).then(
+                response => {
+                    commit('registerSuccess');
+                    return Promise.resolve(response.data);
+                },
+                error => {
+                    commit('registerFailure');
+                    return Promise.reject(error)
+                }
+            );
         }
     }
 };
