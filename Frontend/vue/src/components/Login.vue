6fa4f6eb10277f77a192fcef8d396361f53d61cb
kimoror <kimoror@mail.ru>
2021-07-18 23:51:41 +0300

    Add components and update router.js

--- Frontend/vue/src/components/Login.vue
+++ Frontend/vue/src/components/Login.vue
@@ -1,17 +1,60 @@
-<!--TODO Дописать, но сначала router-->
 <template>
+  <div class="col-md-12">
+    <img id = "profile-img"
+         src = "src/assets/profile-img.png"
+         class="profile-img-card"
+         alt="profile-umg.png"
+    />
+    <Form @submit="handleLogin" :validation-schema="schema">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <Field name="username" type="text" class="form-control"/>
+        <ErrorMessage name="username" class="error-feedback"/>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <Field name="password" type="password" class="form-control" />
+        <ErrorMessage name="password" class="erorr-feeback" />
+      </div>
 
+      <div class="form-group">
+        <button class="btn btn-primary btn-block" :disabled="loading">
+          <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+          <span>Login</span>
+        </button>
+      </div>
+
+      <div class="form-group">
+        <div v-if="message" class="alert alert-danger" role="alert">
+          {{message}}
+        </div>
+      </div>
+    </Form>
+  </div>
 </template>
 
 <script>
-import User from '../models/user'
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
+
 export default {
   name: "Login",
+  components:{
+    Form,
+    Field,
+    ErrorMessage
+  },
   data(){
+    const schema = yup.object.shape({
+      username: yup.string.required("Username is required!"),
+      password: yup.string.required("Password is required!"),
+    });
+
     return{
       user: new User('',''),
       loading: false,
-       message: '',
+      message: '',
+      schema,
     };
   },
   computed:{
@@ -23,9 +66,20 @@ export default {
     }
   },
   methods:{
-    handleLogin(){
+    handleLogin(user){
       this.loading = true;
-      this.$validator.validateAll
+      this.$store.dispatch("auth/login", user).then(
+          () => {
+            this.$router.push("/profile")
+          },
+          (error) => {
+            this.loading = false;
+            this.message =
+                (error.response && error.response.data && error.response.data.message)
+                || error.message
+                || error.toString();
+          }
+      )
     }
   }
 }
